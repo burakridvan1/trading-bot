7279f60e39de43334e50bf33ecb0c432e5de7357
burakridvan1 <burakridvan1@gmail.com>
2026-04-10 14:01:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
 import os
+from dotenv import load_dotenv
 
-TELEGRAM_TOKEN = os.getenv("8789711602:AAEypX4ngAN0XZA2_B4cOB3HRTp5kT5JkVU")
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 SCAN_INTERVAL_MINUTES = 15
+TOP_K = 5
+
+if not TELEGRAM_TOKEN:
+    raise Exception("❌ TELEGRAM_TOKEN eksik (.env dosyasına ekle)")
+
+if not CHAT_ID:
+    raise Exception("❌ CHAT_ID eksik (.env dosyasına ekle)")
